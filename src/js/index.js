6a81ceba89c5341154c10714e69575e3a351533c
rodrigocelmer <rodrigocelmer@hotmail.com>
2022-05-18 21:24:00 -0300

    check if new user already exists + wip messages

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 "use strict";
-const form_access = document.querySelector('#form_access'), form_createAccount = document.querySelector('#form_createAccount');
+const form_access = document.querySelector('#form_access'), form_createAccount = document.querySelector('#form_createAccount'), form_messages = document.querySelector('#form_messages');
 const readLocalStorage = () => {
     const users = JSON.parse(localStorage.getItem('users') || '[]');
     return users;
@@ -7,7 +7,13 @@ const readLocalStorage = () => {
 const addNewUser = (event) => {
     event === null || event === void 0 ? void 0 : event.preventDefault();
     const newUserName = form_createAccount === null || form_createAccount === void 0 ? void 0 : form_createAccount.input_createUser.value, newUserPassword = form_createAccount === null || form_createAccount === void 0 ? void 0 : form_createAccount.input_createPassword.value, repeatPassword = form_createAccount === null || form_createAccount === void 0 ? void 0 : form_createAccount.input_repeatPassword.value, users = readLocalStorage();
-    if (newUserPassword !== repeatPassword) {
+    const userFind = users.find(user => user.userName === newUserName);
+    if (userFind) {
+        alert(`O usuário ${newUserName} já existe!`);
+        form_createAccount.reset();
+        return 0;
+    }
+    else if (newUserPassword !== repeatPassword) {
         alert("As senhas inseridas não condizem!");
         form_createAccount.reset();
         return 0;
@@ -30,5 +36,17 @@ const accessInbox = (event) => {
         alert("Usuário ou senha inválidos!");
     }
 };
+const saveMessage = (event) => {
+    event === null || event === void 0 ? void 0 : event.preventDefault();
+    const newDescription = form_messages === null || form_messages === void 0 ? void 0 : form_messages.input_msgDescr.value, newDetails = form_messages === null || form_messages === void 0 ? void 0 : form_messages.input_msgDetails.value, messages = JSON.parse(localStorage.getItem('messages') || '[]');
+    messages.push({
+        userName: "user namesson",
+        description: newDescription,
+        details: newDetails
+    });
+    localStorage.setItem('messages', JSON.stringify(messages));
+    form_messages.reset();
+};
 form_access === null || form_access === void 0 ? void 0 : form_access.addEventListener('submit', accessInbox);
 form_createAccount === null || form_createAccount === void 0 ? void 0 : form_createAccount.addEventListener('submit', addNewUser);
+form_messages.addEventListener('submit', saveMessage);
--- src/js/index.ts
+++ src/js/index.ts
@@ -1,10 +1,18 @@
 const   form_access         = document.querySelector('#form_access') as HTMLFormElement,
-        form_createAccount  = document.querySelector('#form_createAccount') as HTMLFormElement;
+        form_createAccount  = document.querySelector('#form_createAccount') as HTMLFormElement,
+        form_messages       = document.querySelector('#form_messages') as HTMLFormElement;
 
 interface User{
     // id:             number;
     userName:       string;
     userPassword:   string;
+}
+
+interface UserMessages{
+    // id:             number;
+    userName:       string;
+    description:    string;
+    details:        string;
 }
 
 const readLocalStorage = (): Array<User> => {
@@ -21,7 +29,16 @@ const addNewUser = (event: Event) => {
             repeatPassword  = form_createAccount?.input_repeatPassword.value,
             users           = readLocalStorage();
 
-    if(newUserPassword !== repeatPassword)
+    const userFind = users.find(user => 
+        user.userName === newUserName
+    )
+
+    if(userFind)
+    {
+        alert(`O usuário ${newUserName} já existe!`)
+        form_createAccount.reset();
+        return 0;
+    }else if(newUserPassword !== repeatPassword)
     {
         alert("As senhas inseridas não condizem!")
         form_createAccount.reset();
@@ -59,5 +76,24 @@ const accessInbox = (event: Event) => {
     }
 }
 
+const saveMessage = (event: Event) => {
+    event?.preventDefault();
+
+    const   newDescription  = form_messages?.input_msgDescr.value,
+            newDetails      = form_messages?.input_msgDetails.value,
+            messages        = JSON.parse(localStorage.getItem('messages') || '[]') as Array<UserMessages>;
+
+    messages.push({
+        userName:       "user namesson",
+        description:    newDescription,
+        details:        newDetails
+    })
+
+    localStorage.setItem('messages', JSON.stringify(messages))
+
+    form_messages.reset()
+}
+
 form_access?.addEventListener('submit', accessInbox);
 form_createAccount?.addEventListener('submit', addNewUser);
+form_messages.addEventListener('submit', saveMessage);
