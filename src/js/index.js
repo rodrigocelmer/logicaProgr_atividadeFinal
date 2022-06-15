adc6c9c10f59e83789c9fd9bf7be2ad8bd07f2ba
rodrigocelmer <rodrigocelmer@hotmail.com>
2022-06-15 19:45:19 -0300

    fix submit forms issue

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,23 @@
 "use strict";
-const form_access = document.querySelector('#form_access'), form_createAccount = document.querySelector('#form_createAccount');
+const form_access = document.querySelector('#form_access'), form_createAccount = document.querySelector('#form_createAccount'), form_messages = document.querySelector('#form_messages'), tbody = document.querySelector('#tbody');
+const readLocalStorage = (option) => {
+    let infoLocalStorage;
+    switch (option) {
+        case 'users':
+            infoLocalStorage = JSON.parse(localStorage.getItem('users') || '[]');
+            break;
+        case 'messages':
+            infoLocalStorage = JSON.parse(localStorage.getItem('messages') || '[]');
+            break;
+        case 'userLogged':
+            infoLocalStorage = JSON.parse(localStorage.getItem('userLogged') || '');
+            break;
+        default:
+            infoLocalStorage = [];
+            break;
+    }
+    return infoLocalStorage;
+};
 const addNewUser = (event) => {
     event === null || event === void 0 ? void 0 : event.preventDefault();
     console.log("addNewUser");
@@ -29,14 +47,91 @@ const accessInbox = (event) => {
     console.log("accessInbox");
     const accessUserName = form_access === null || form_access === void 0 ? void 0 : form_access.input_userName.value, accessPassword = form_access === null || form_access === void 0 ? void 0 : form_access.input_password.value, users = readLocalStorage('users');
     const userFind = users.find(user => user.userName === accessUserName);
-    if (!userFind || (userFind.userPassword !== accessPassword)) {
+    if (userFind && (userFind.userPassword === accessPassword)) {
+        document.location.href = './messages.html';
+    }
+    else {
         alert("Usuário ou senha inválidos!");
         form_access.reset();
-        return;
+        return 0;
     }
-    document.location.href = './messages.html';
     localStorage.setItem('userLogged', JSON.stringify(userFind.userName));
     showUserMessages();
 };
+const saveMessage = (event) => {
+    event === null || event === void 0 ? void 0 : event.preventDefault();
+    const newDescription = form_messages === null || form_messages === void 0 ? void 0 : form_messages.input_msgDescr.value, newDetails = form_messages === null || form_messages === void 0 ? void 0 : form_messages.input_msgDetails.value, messages = readLocalStorage('messages'), userLogged = readLocalStorage('userLogged');
+    messages.push({
+        id: defineID() + 1,
+        userName: userLogged,
+        description: newDescription,
+        details: newDetails
+    });
+    localStorage.setItem('messages', JSON.stringify(messages));
+    form_messages.reset();
+    showUserMessages();
+};
+const showUserMessages = () => {
+    const messages = readLocalStorage('messages'), userLogged = readLocalStorage('userLogged');
+    if (tbody) {
+        tbody.innerHTML = "";
+    }
+    for (const message of messages) {
+        if (message.userName === userLogged) {
+            tbody.innerHTML += `
+                <tr>
+                    <td>${message.id}</td>
+                    <td>${message.description}</td>
+                    <td>${message.details}</td>
+                    <td>
+                        <button class="editDeleteBtn" onclick="editMessage(${message.id})">Editar</button>
+                        <button class="editDeleteBtn" onclick="deleteMessage(${message.id})">Apagar</button>
+                    </td>
+                </tr>
+            `;
+        }
+    }
+};
+const deleteMessage = (id) => {
+    const messages = readLocalStorage('messages');
+    const msgIndex = messages.findIndex((message) => message.id === id);
+    messages.splice(msgIndex, 1);
+    localStorage.setItem('messages', JSON.stringify(messages));
+    showUserMessages();
+};
+const editMessage = (id) => {
+    const messages = readLocalStorage('messages');
+    const msgIndex = messages.findIndex((message) => message.id === id);
+    const editOption = Number(prompt("Escolha a opção para editar \n[1] - Descrição\n[2] - Detalhamento\n[3] - Ambos"));
+    switch (editOption) {
+        case 1:
+            messages[msgIndex].description = prompt("Informe a nova descrição.");
+            break;
+        case 2:
+            messages[msgIndex].details = prompt("Informe o novo detalhamento.");
+            break;
+        case 3:
+            messages[msgIndex].description = prompt("Informe a nova descrição.");
+            messages[msgIndex].details = prompt("Informe o novo detalhamento.");
+            break;
+        default:
+            alert("Opção inválida!");
+            break;
+    }
+    localStorage.setItem('messages', JSON.stringify(messages));
+    showUserMessages();
+};
+const defineID = () => {
+    const messages = readLocalStorage('messages'), userLogged = readLocalStorage('userLogged');
+    let max = 0;
+    messages.forEach((message) => {
+        if ((message.id > max)) {
+            max = message.id;
+        }
+    });
+    return max;
+};
 form_access === null || form_access === void 0 ? void 0 : form_access.addEventListener('submit', accessInbox);
 form_createAccount === null || form_createAccount === void 0 ? void 0 : form_createAccount.addEventListener('submit', addNewUser);
+form_messages === null || form_messages === void 0 ? void 0 : form_messages.addEventListener('submit', saveMessage);
+document.addEventListener('DOMContentLoaded', showUserMessages);
--- src/js/index.ts
+++ src/js/index.ts
@@ -1,9 +1,40 @@
 const   form_access         = document.querySelector('#form_access') as HTMLFormElement,
-        form_createAccount  = document.querySelector('#form_createAccount') as HTMLFormElement;
+        form_createAccount  = document.querySelector('#form_createAccount') as HTMLFormElement,
+        form_messages       = document.querySelector('#form_messages') as HTMLFormElement,
+        tbody               = document.querySelector('#tbody') as HTMLElement;
 
 interface User{
     userName:       string;
     userPassword:   string;
+}
+
+interface UserMessages{
+    id:             number;
+    userName:       string;
+    description:    string;
+    details:        string;
+}
+
+const readLocalStorage = (option: string): Array<User> | Array<UserMessages> | string => {
+    let infoLocalStorage: Array<User> | Array<UserMessages> | string;
+
+    switch(option)
+    {
+        case 'users':
+            infoLocalStorage = JSON.parse(localStorage.getItem('users') || '[]') as Array<User>;
+        break;
+        case 'messages':
+            infoLocalStorage = JSON.parse(localStorage.getItem('messages') || '[]') as Array<UserMessages>;
+        break;
+        case 'userLogged':
+            infoLocalStorage = JSON.parse(localStorage.getItem('userLogged') || '') as string;
+        break;
+        default:
+            infoLocalStorage = [];
+        break;
+    }
+
+    return infoLocalStorage;
 }
 
 const addNewUser = (event: Event) => {
@@ -59,17 +90,127 @@ const accessInbox = (event: Event) => {
         user.userName === accessUserName
     )
 
-    if(!userFind || (userFind.userPassword !== accessPassword))
+    if(userFind && (userFind.userPassword === accessPassword))
+    {
+        document.location.href = './messages.html'
+    }
+    else
     {
         alert("Usuário ou senha inválidos!")   
         form_access.reset();
-        return;
+        return 0
     }
 
-    document.location.href = './messages.html'
     localStorage.setItem('userLogged', JSON.stringify(userFind.userName));
+    
     showUserMessages();
+}
+
+const saveMessage = (event: Event) => {
+    event?.preventDefault();
+
+    const   newDescription  = form_messages?.input_msgDescr.value,
+            newDetails      = form_messages?.input_msgDetails.value,
+            messages        = readLocalStorage('messages') as Array<UserMessages>,
+            userLogged      = readLocalStorage('userLogged') as string;
+
+    messages.push({
+        id:             defineID() + 1,
+        userName:       userLogged,
+        description:    newDescription,
+        details:        newDetails
+    })
+
+    localStorage.setItem('messages', JSON.stringify(messages))
+
+    form_messages.reset()
+
+    showUserMessages();
+}
+
+const showUserMessages = () => {
+    const   messages    = readLocalStorage('messages') as Array<UserMessages>,
+            userLogged  = readLocalStorage('userLogged') as string;
+
+    if(tbody)
+    {
+        tbody.innerHTML = ""
+    }
+
+    for(const message of messages)
+    {
+        if(message.userName === userLogged)
+        {
+            tbody.innerHTML += `
+                <tr>
+                    <td>${message.id}</td>
+                    <td>${message.description}</td>
+                    <td>${message.details}</td>
+                    <td>
+                        <button class="editDeleteBtn" onclick="editMessage(${message.id})">Editar</button>
+                        <button class="editDeleteBtn" onclick="deleteMessage(${message.id})">Apagar</button>
+                    </td>
+                </tr>
+            `;
+        }
+    }
+}
+
+const deleteMessage = (id: number) => {
+    const messages = readLocalStorage('messages') as Array<UserMessages>;
+
+    const msgIndex = messages.findIndex((message) => message.id === id);
+
+    messages.splice(msgIndex, 1);
+
+    localStorage.setItem('messages', JSON.stringify(messages))
+
+    showUserMessages();
+}
+
+const editMessage = (id: number) => {
+    const messages = readLocalStorage('messages') as Array<UserMessages>;
+
+    const msgIndex = messages.findIndex((message) => message.id === id);
+
+    const editOption = Number(prompt("Escolha a opção para editar \n[1] - Descrição\n[2] - Detalhamento\n[3] - Ambos"))
+
+    switch(editOption)
+    {
+        case 1:
+            messages[msgIndex].description = prompt("Informe a nova descrição.") as string;
+        break
+        case 2:
+            messages[msgIndex].details = prompt("Informe o novo detalhamento.") as string;
+        break
+        case 3:
+            messages[msgIndex].description = prompt("Informe a nova descrição.") as string;
+            messages[msgIndex].details = prompt("Informe o novo detalhamento.") as string;
+        break
+        default:
+            alert("Opção inválida!");
+        break
+    }
+    
+    localStorage.setItem('messages', JSON.stringify(messages))
+    showUserMessages();
+}
+
+const defineID = (): number => {
+    const   messages    = readLocalStorage('messages') as Array<UserMessages>,
+            userLogged  = readLocalStorage('userLogged') as string;
+    let     max         = 0;
+    
+    messages.forEach((message) => {
+        if((message.id > max)){
+            max = message.id;
+        }
+    })
+
+    return max
 }
 
 form_access?.addEventListener('submit', accessInbox);
 form_createAccount?.addEventListener('submit', addNewUser);
+form_messages?.addEventListener('submit', saveMessage);
+document.addEventListener('DOMContentLoaded', showUserMessages);
